ae79d92ec902f7c93748f3575492a36f9cc27b37
Lukas Trisauskas <Lukast4991@outlook.com>
2021-06-11 00:08:38 +0100

    graph.py

---
+++ graph.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+
+
+from bokeh.models import NumeralTickFormatter, tools
+from bokeh.plotting import figure, show
+
+df = pd.read_csv('../cv2/data.csv')
+x = np.arange(0,100)
+y = df['score']
+
+# create new plot
+p = figure(plot_width=1400, plot_height=350, tools="xpan, save")
+p.y_range.flipped = True
+# add renderers
+p.circle(x, y, size=8)
+p.line(x, y, color="navy", line_width=1)
+
+# format axes ticks
+p.yaxis[0].formatter = NumeralTickFormatter(format="0.000")
+
+# show the results
+show(p)
