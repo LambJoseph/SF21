9374dd7c0fa199a7183645bb4d73378808eca92e
Lukas Trisauskas <Lukast4991@outlook.com>
2021-06-14 22:46:11 +0100

    selected data is rendered

--- graph.py
+++ graph.py
@@ -1,81 +1,33 @@
 
+
+from bokeh.core.property.visual import FontSize
 import numpy as np
 import pandas as pd
-import time
-
-from bokeh.layouts import column
-from bokeh.models import ColumnDataSource, RangeTool, BoxAnnotation, CustomJS, BoxSelectTool
-from bokeh.plotting import figure, show
-from bokeh.events import SelectionGeometry
-
+from bokeh import events
+from bokeh.layouts import column, row
+from bokeh.models import ColumnDataSource, RangeTool, BoxSelectTool, PreText
+from bokeh.plotting import figure, curdoc, show
 df = pd.read_csv('../cv2/data.csv')
 
-source = ColumnDataSource(data=dict(x=np.arange(0, len(df)), y=df['score'], image_path=df['path'], frame=df['frame']))
-selected_data = ColumnDataSource(data=dict(frame=[], y=[], image_path=[]))
-
+s1 = ColumnDataSource(data=dict(x=np.arange(0, len(df)), y=df['score'], image_path=df['path'], frame=df['frame']))
+s2 = ColumnDataSource(data=dict(frame=[], y=[], image_path=[]))
 
 TOOLTIPS = """ """
 with open('hover.html', 'r') as f:
     TOOLTIPS = f.read()
 
 # create new plot
-p = figure(plot_width=1100, plot_height=500, 
-           tools="xpan, save, zoom_in, zoom_out, ypan", 
-           x_range=(0, 100), tooltips=TOOLTIPS)
+p = figure(plot_width=1100, plot_height=500, tools="xpan, save, zoom_in, zoom_out, ypan", x_range=(0, 100), tooltips=TOOLTIPS)
 p.y_range.flipped = True
-# add renderers
-p.line(x='x', y='y', source=source, color="blue", line_width=1, line_alpha=0.4)
-### range tool ###
-peek = figure(plot_height=70, plot_width=1100, y_range=p.y_range, x_axis_type='linear', y_axis_type=None,
-                tools="", toolbar_location=None, background_fill_color="white")
-
-callback = CustomJS(args=dict(source=source, selected_data=selected_data), code="""
-    console.log("selection event");
-    
-    var g = cb_obj['geometry'];
-    
-    var start = Math.round(g['x0']);
-    var end = Math.round(g['x1']);
-    
-    var d = source.data;
-    var sf = selected_data;
-    
-    var f = sf['frame'];
-    var y = sf['y'];
-    var i = sf['image_path']
-    
-    frame = [];
-    y = [];
-    image_path = [];
-    
-    var selected_frame = d['frame'].slice(start, end);
-    var selected_y = d['y'].slice(start, end);
-    var selected_image_path = d['image_path'].slice(start, end);
-
-    frame.push(selected_frame);
-    y.push(selected_y);
-    image_path.push(selected_image_path);
-    
-    console.log(frame);
-    console.log(y);
-    console.log(image_path);
-     
-""")
-
-
-
-p.js_on_event('selectiongeometry', callback)
-
-
-
+p.line(x='x', y='y', source=s1, color="blue", line_width=1, line_alpha=0.4)
+peek = figure(plot_height=70, plot_width=1100, y_range=p.y_range, x_axis_type='linear', y_axis_type=None, background_fill_color="white")
 
 box_select = BoxSelectTool(dimensions="width")
 box_select.overlay.line_dash =  "solid"
 box_select.overlay.line_width = 1
 box_select.overlay.fill_color = "green"
-box_select.overlay.fill_alpha = 0.2
+box_select.overlay.fill_alpha = 0.1
 p.add_tools(box_select)
-
 
 range_tool = RangeTool(x_range=p.x_range)
 range_tool.overlay.fill_color = "white"
@@ -83,7 +35,7 @@ range_tool.overlay.fill_alpha = 0.1
 range_tool.overlay.line_dash = 'solid'
 range_tool.overlay.line_width = 1
 
-peek.line('x', 'y', source=source, color="grey", line_alpha=0.4)
+peek.line('x', 'y', source=s1, color="grey", line_alpha=0.4)
 peek.add_tools(range_tool)
 peek.toolbar.active_multi = range_tool
 
@@ -93,9 +45,28 @@ peek.ygrid.line_color = 'whitesmoke'
 p.ygrid.minor_grid_line_color = 'navy'
 p.ygrid.minor_grid_line_alpha = 0.1
 
+stats = PreText(text="", width=500, FontSize="14px")
 
+def update_stats():
+    stats.text = pd.DataFrame(s2.data).to_string()
 
-# show the results
-layout = column(p, peek)
+def selection_change(event):
+    event_data = event.geometry
+    start = round(event_data['x0'])
+    end   = round(event_data['x1'])
+    d1 = s1.data
 
-show(layout)
+    new_data = {
+        'frame': d1['frame'][start:end], 
+        'y': d1['y'][start:end],
+        'image_path': d1['image_path'][start:end]
+    }
+    s2.stream(new_data, rollover=26)
+    update_stats()
+p.on_event('selectiongeometry', selection_change)
+
+r1 = row(p, stats)
+c1 = column(peek)
+c2 = column(r1, c1)
+
+curdoc().add_root(c2)
